6c1a7869b8cc4e15f5870811e7ae67e251da5285
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2021-11-26 01:01:59 +0100

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        [Key]
+        public int ContentID { get; set; }
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+
+        //ContentWriter
+        //ContentHeading
+
+        //Heading ile iliski
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        // Writer ile iliski
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        [Key]
+        public int HeadingID { get; set; }
+        [StringLength(50)]
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+        
+        //Category ile iliski olacak
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        //Writer ile iliski olacak
+
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+
+        //Content ile iliski olacak
+
+        public ICollection<Content> Contents { get; set; }
+    }
+}
