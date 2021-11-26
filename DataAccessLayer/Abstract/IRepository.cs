868507cfa71cda0323e243afa0047c719510f62a
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2021-11-26 20:08:24 +0100

    Creating interfaces and generic repository

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        //ADD
+        void Insert(T p);
+        //Update
+        void Update(T p);
+        //Delete
+        void Delete(T p);
+
+        List<T> list(Expression<Func<T, bool>> filter);
+    }
+}
