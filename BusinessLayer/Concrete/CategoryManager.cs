bd5f9853e93f4139858ea312930d7d3351eaf8e4
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2021-12-03 13:01:08 +0100

    BusinessLayer and bootstrap settings

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    { 
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" ||
+                p.CategoryName.Length >= 51)
+            {
+                //error message
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+    
+}
+
